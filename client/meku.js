d8bf906a8b83b5c1f85447426f1730939d416bec
Antti Holvikari <antti.holvikari@reaktor.fi>
2014-04-07 16:36:59 +0300

    Save more data

--- client/meku.js
+++ client/meku.js
@@ -1,17 +1,31 @@
 $(setup)
 
 function setup() {
+  $.ajaxSetup({dataType: "json", processData: false, contentType: "application/json"})
+
+  var $form = $('#movie-details')
+
   $('.new-movie').click(function() {
     $.post('/movies/new').done(function(movie) {
       $('.new-movie').hide()
-      $('#movie-details').show()
+      $form.show().data('id', movie._id)
+    })
+  })
+
+  $form.find("input").throttledInput(function(txt) {
+    $.ajax({
+      type: 'POST',
+      url: '/movies/' + $form.data('id'),
+      data: JSON.stringify(keyValue($(this).attr('name'), txt))
     })
   })
 }
 
-
-
-
+function keyValue(key, value) {
+  var data = {}
+  data[key] = value
+  return data
+}
 
 $.fn.throttledInput = function(fn) {
   return $(this).each(function() {
@@ -19,11 +33,12 @@ $.fn.throttledInput = function(fn) {
     var timeout = null
     var $input = $(this).on('keyup', function() {
       var txt = $input.val()
+      var that = this
       if (timeout) clearTimeout(timeout)
       timeout = setTimeout(function() {
         if (prev == txt) return
         prev = txt
-        fn(txt)
+        fn.call(that, txt)
       }, 400)
     })
   })

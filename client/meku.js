45a2133b50ed0e9318b0ca7d47adf87cb3a8267a
Mikko Koponen <mikko.koponen@reaktor.fi>
2014-04-08 11:48:26 +0300

    Typeahead for country selection

--- client/meku.js
+++ client/meku.js
@@ -21,6 +21,11 @@ function movieDetails() {
     })
   })
 
+  $form.find('input.country').typeahead(
+    { hint: true, highlight: true, minLength: 1 },
+    { name: 'countries', displayKey: 'value', source: countryMatcher() }
+  )
+
   $form.find('input').throttledInput(function(txt) {
     saveMovieField($form.data('id'), $(this).attr('name'), txt)
   })
@@ -74,4 +79,13 @@ $.fn.throttledInput = function(fn) {
       }, 400)
     })
   })
+}
+
+function countryMatcher() {
+  var countries = ['Afganistan', 'Alankomaat', 'Albania', 'Algeria', 'Arabiemiirikunnat', 'Argentiina', 'Australia', 'Bangladesh', 'Belgia', 'Bermuda', 'Bhutan', 'Bhutan', 'Bolivia', 'Bosnia-Hertsegovina', 'Brasilia', 'Bulgaria', 'Chile', 'Costa Rica', 'Ecuador', 'Egypti', 'El Salvador', 'Espanja', 'Etelä-Afrikka', 'Etelä-Korea', 'Etiopia', 'Filippiinit', 'Fär-saaret', 'Grönlanti', 'Guatemala', 'Guinea', 'Hongkong', 'Indonesia', 'Intia', 'Irak', 'Iran', 'Irlanti', 'Islanti', 'Iso-Britannia', 'Israel', 'Italia', 'Itä-Saksa', 'Itävalta', 'Jamaika', 'Japani', 'Jordania', 'Jugoslavia', 'Kamerun', 'Kanada', 'Kenia', 'Kiina', 'Kolumbia', 'Kreikka', 'Kroatia', 'Kuuba', 'Kypros', 'Laos', 'Latvia', 'Libanon', 'Lichtenstein', 'Liettua', 'Luxemburg', 'Malesia', 'Mali', 'Malta', 'Marokko', 'Mauritania', 'Mauritius', 'Meksiko', 'Monaca', 'Mongolia', 'Mosambik', 'Muu maa', 'Namibia', 'Nepal', 'Neuvostoliitto', 'Nicaragua', 'Niger', 'Nigeria', 'Norja', 'Pakistan', 'Paraguay', 'Peru', 'Pohjois-Korea', 'Portugali', 'Puerto Rico', 'Puola', 'Ranska', 'Romania', 'Ruotsi', 'Saksa', 'Sambia', 'Senegal', 'Serbia / Tsekkoslovakia', 'Singapore', 'Slovakia', 'Slovenia', 'Sri Lanka', 'Sudan', 'Suomi', 'Sveitsi', 'Syyria', 'Taiwan', 'Tansania', 'Tanska', 'Thaimaa', 'Tsekinmaa', 'Tunisia', 'Turkki', 'Unkari', 'Uruguay', 'Uusi-Seelanti', 'Valko-Venäjä', 'Vatikaanivaltio', 'Venezuela', 'Venäjä', 'Vietnam', 'Viro', 'Yhdysvallat', 'Zimbabwe']
+  return function(q, callback) {
+    var regexp = new RegExp('(^| )' + q, 'i')
+    var result = countries.filter(function(s) { return regexp.test(s) }).map(function(s) { return { value: s } })
+    callback(result)
+  }
 }

e64c864ec9cef105f0ea664e914cffbb8fdfa8b2
Antti Holvikari <antti.holvikari@reaktor.fi>
2014-04-14 12:39:38 +0300

    Move data importing to scripts/import.js and Mongoose schema to server/schema.js

--- server/app.js
+++ server/app.js
@@ -2,41 +2,15 @@ var express = require('express')
 var path = require('path')
 var mongoose = require('mongoose')
 var liveReload = require('express-livereload')
+var schema = require('server/schema')
+var Movie = schema.Movie
+var ProductionCompany = schema.ProductionCompany
 
 var app = express()
 
 app.use(express.json())
 
 mongoose.connect('mongodb://localhost/meku')
-
-var classification = {
-  author: String,
-  buyer: String,
-  billing: String,
-  format: String,
-  duration: String, // for now matches a regexp in the client
-  safe: Boolean,
-  criteria: [Number],
-  comments: {}
-}
-
-var Movie = mongoose.model('movies', {
-  name: String,
-  'name-fi': String,
-  'name-sv': String,
-  country: String,
-  year: Number,
-  'production-companies': [{_id: mongoose.Schema.Types.ObjectId, name: {type: String, index: true}}],
-  genre: String,
-  directors: [String],
-  actors: [String],
-  synopsis: String,
-  classifications: [classification]
-})
-
-var ProductionCompany = mongoose.model('production_companies', {
-  name: String
-})
 
 app.get('/movies/:id', function(req, res) {
   Movie.findById(req.params.id, function(err, movie) { res.send(movie) })
@@ -75,23 +49,5 @@ liveReload(app, { watchDir: path.join(__dirname, '../client') })
 
 var server = app.listen(3000, function() {
   console.log('Listening on port ' + server.address().port)
-  importProductionCompanies()
 })
 
-function importProductionCompanies() {
-  ProductionCompany.remove({}, function(err) { })
-
-  // update production companies
-  var fs = require('fs')
-  fs.readFile(path.join(__dirname, '../data/meku-production-companies-samples.txt'), {encoding: 'utf8'}, function(err, data) {
-    if (err) throw err;
-
-    var companies = data.split('\n').filter(function (x) { return x.length > 0 })
-
-    companies.forEach(function(name) {
-      new ProductionCompany({name: name}).save(function(err, comp) {
-        if (err) throw err
-      })
-    })
-  })
-}
---
+++ server/schema.js
@@ -0,0 +1,35 @@
+var mongoose = require('mongoose')
+
+var classification = {
+  author: String,
+  buyer: String,
+  billing: String,
+  format: String,
+  duration: String, // for now matches a regexp in the client
+  safe: Boolean,
+  criteria: [Number],
+  comments: {}
+}
+
+var Movie = exports.Movie = mongoose.model('movies', {
+  name: String,
+  'name-fi': String,
+  'name-sv': String,
+  country: String,
+  year: Number,
+  'production-companies': [{_id: mongoose.Schema.Types.ObjectId, name: {type: String, index: true}}],
+  genre: String,
+  directors: [String],
+  actors: [String],
+  synopsis: String,
+  classifications: [classification]
+})
+
+var ProductionCompany = exports.ProductionCompany = mongoose.model('production_companies', {
+  name: String
+})
+
+//var Account = mongoose.model('accounts', {
+//  name: String
+//})
+

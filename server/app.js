b363965981019e1612535aed89575d4f376cb0b6
Antti Holvikari <antti.holvikari@reaktor.fi>
2014-04-16 12:00:06 +0300

    Must type at least three characters for the actors search

--- server/app.js
+++ server/app.js
@@ -44,16 +44,19 @@ app.get('/accounts/:query', function(req, res, next) {
 })
 
 app.get('/actors/:query', function(req, res, next) {
-  Movie.aggregate([
-    {$unwind: '$actors'},
-    {$match: {actors: new RegExp("\\b" + req.params.query, 'i')}},
-    {$project: {actors: 1}},
-    {$group: {_id: "$actors"}}
-  ]).exec(function(err, data) {
-      return res.send(data.reduce(function(acc, doc) {
-        return acc.concat([doc._id])
-      }, []))
-    })
+  if (req.params.query.length < 3) return res.send([])
+  else {
+    Movie.aggregate([
+      {$unwind: '$actors'},
+      {$match: {actors: new RegExp("\\b" + req.params.query, 'i')}},
+      {$project: {actors: 1}},
+      {$group: {_id: "$actors"}}
+    ]).exec(function(err, data) {
+        return res.send(data.reduce(function(acc, doc) {
+          return acc.concat([doc._id])
+        }, []))
+      })
+  }
 })
 
 app.use(express.static(path.join(__dirname, '../client')))

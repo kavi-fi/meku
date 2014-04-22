443f1ffd8c647b4cd6abceabe72d5e3e66e5baf8
Antti Holvikari <antti.holvikari@reaktor.fi>
2014-04-22 13:32:21 +0300

    Remove account sample data import code

--- scripts/import.js
+++ scripts/import.js
@@ -7,7 +7,7 @@ var Account = schema.Account
 
 mongoose.connect('mongodb://localhost/meku')
 
-async.series([importProductionCompanies, importAccounts], function() {
+async.series([importProductionCompanies], function() {
   mongoose.disconnect()
 })
 
@@ -24,21 +24,6 @@ function importProductionCompanies(callback) {
 
     async.each(companies, function(name, next) {
       new ProductionCompany({name: name}).save(next)
-    }, callback)
-  })
-}
-
-function importAccounts(callback) {
-  Account.remove({}, function(err) { })
-
-  var fs = require('fs')
-  fs.readFile(path.join(__dirname, '../data/meku-production-companies-samples.txt'), {encoding: 'utf8'}, function(err, data) {
-    if (err) throw err;
-
-    var accounts = data.split('\n').filter(function (x) { return x.length > 0 })
-
-    async.each(accounts, function(name, next) {
-      new Account({name: name}).save(next)
     }, callback)
   })
 }

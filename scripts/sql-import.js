de2e468e8ad2188e5eae6d2f0036c226b9b7c05e
Mikko Koponen <mikko.koponen@reaktor.fi>
2014-05-23 13:19:22 +0300

    Cleanup sql-import
    * Common functions for common tasks
    * Group smaller imports

--- scripts/sql-import.js
+++ scripts/sql-import.js
@@ -9,29 +9,28 @@ var stream = require('stream')
 var _ = require('lodash')
 var conn = mysql.createConnection({ host: 'localhost', user:'root', database: 'emeku' })
 
-// Remove programs which have no classifications? -> ~7400
-
 /*
-Dependencies:
-  base & names before everything
-  providers & accounts & users before classifications
-  linkTvSeries before nameIndex
+ Dependencies:
+   base & names before everything
+   accounts before classifications
+   linkTvSeries before nameIndex
+
+ There are some memory issues, so run in parts, eg:
+   node scripts/sql-import.js base && node scripts/sql-import.js names && node scripts/sql-import.js metadata accounts classifications markTrainingProgramsDeleted linkTvSeries nameIndex
+
+ Should remove programs which have no classifications? -> ~7400
+   check how many are tv-series-names?
 */
 
 var tasks = {
   wipe: wipe, base: base,
+  wipeMetadata: wipeMetadata, metadata: metadata,
   wipeNames: wipeNames, names: names,
-  wipeActors: wipeActors, actors: actors,
-  wipeDirectors: wipeDirectors, directors: directors,
-  wipeProductionCompanies: wipeProductionCompanies, productionCompanies: productionCompanies,
   wipeClassifications: wipeClassifications, classifications: classifications,
-  wipeProviders: wipeProviders, providers: providers,
   wipeAccounts: wipeAccounts, accounts: accounts,
-  wipeUsers: wipeUsers, users: users,
   nameIndex: nameIndex,
   markTrainingProgramsDeleted: markTrainingProgramsDeleted,
-  linkTvSeries: linkTvSeries,
-  demoUsers: demoUsers
+  linkTvSeries: linkTvSeries
 }
 
 if (process.argv.length < 3) {
@@ -63,36 +62,43 @@ function run(job, callback) {
 }
 
 function base(callback) {
-  conn.query('SELECT id, program_type, publish_year, year, countries, description, genre, tv_program_genre, game_genre, game_format FROM meku_audiovisualprograms where deleted != "1"')
-    .stream({ highWaterMark: 2000 })
-    .pipe(programMapper())
-    .pipe(batcher(1000))
-    .pipe(batchInserter('Movie', callback))
+  var q = 'SELECT id, program_type, publish_year, year, countries, description, genre, tv_program_genre, game_genre, game_format FROM meku_audiovisualprograms where deleted != "1"'
+  batchInserter(q, programMapper, 'Movie', callback)
+
+  function programMapper(row) {
+    var obj = { 'emeku-id': row.id }
+    obj['program-type'] = legacyProgramTypes[row.program_type] || 0
+    if (row.publish_year && row.publish_year != 'undefined') obj.year = row.publish_year
+    if (row.year && row.year != 'undefined') obj.year = row.year
+    if (row.countries) obj.country = optionListToArray(row.countries)
+    if (row.description) obj.synopsis = trim(row.description)
+    var legacyGenre = optionListToArray(row.genre).map(function(g) { return legacyGenres[g] })
+      .concat(optionListToArray(row.tv_program_genre).map(function(g) { return legacyTvGenres[g] }))
+      .concat(optionListToArray(row.game_genre))
+    if (legacyGenre) obj['legacy-genre'] = _(legacyGenre).compact().uniq().value()
+    if (row.program_type == '11' || row.program_type == '08') obj.gameFormat = row.game_format
+    return obj
+  }
 }
 
-// NOTE: Should add meku_audiovisualprograms.name somehow?
 function names(callback) {
-  var tick = progressMonitor()
-  var result = {}
-  conn.query('select p.id, j.type, n.name from meku_audiovisualprograms p join meku_audiovs_meku_names_c j on (p.id = j.meku_audio91farograms_ida) join meku_names n on (j.meku_audio7e24u_names_idb = n.id) where p.deleted != "1" and j.deleted != "1" and n.deleted != "1" and j.type in ("1A", "1B", "2S", "3R", "4M")')
-    .stream()
-    .pipe(consumer(function(row, done) {
-      tick()
-      var name = nameType(row.type)
-      if (!result[row.id]) result[row.id] = {}
-      if (!result[row.id][name]) result[row.id][name] = []
-      var trimmed = trim(row.name)
-      var arr = result[row.id][name]
-      if (arr.indexOf(trimmed) == -1) arr.push(trimmed)
-      done()
-    }, function() {
-      async.eachLimit(Object.keys(result), 5, function(key, cb) {
-        tick('*')
-        var names = result[key]
-        if (!names.name) fixNameless(names)
-        schema.Movie.update({ 'emeku-id': key }, names, cb)
-      }, callback)
-    }))
+  // NOTE: Should add meku_audiovisualprograms.name somehow?
+  var q = 'select p.id, j.type, n.name from meku_audiovisualprograms p join meku_audiovs_meku_names_c j on (p.id = j.meku_audio91farograms_ida) join meku_names n on (j.meku_audio7e24u_names_idb = n.id) where p.deleted != "1" and j.deleted != "1" and n.deleted != "1" and j.type in ("1A", "1B", "2S", "3R", "4M")'
+  batchUpdater(q, mapper, update, callback)
+
+  function mapper(row, result) {
+    var name = nameType(row.type)
+    if (!result[row.id]) result[row.id] = {}
+    if (!result[row.id][name]) result[row.id][name] = []
+    var trimmed = trim(row.name)
+    var arr = result[row.id][name]
+    if (arr.indexOf(trimmed) == -1) arr.push(trimmed)
+  }
+
+  function update(key, obj, callback) {
+    if (!obj.name) fixNameless(obj)
+    schema.Movie.update({ 'emeku-id': key }, obj, callback)
+  }
 
   function nameType(type) {
     if (type == '1A') return 'name'
@@ -108,62 +114,35 @@ function names(callback) {
   }
 }
 
-function actors(callback) {
-  var tick = progressMonitor()
-  var result = {}
-  conn.query('select p.id, CONCAT_WS(" ", TRIM(n.name), TRIM(n.surname)) as name from meku_audiovisualprograms p join meku_audiov_meku_actors_c j on (p.id = j.meku_audio7d9crograms_ida) join meku_actors n on (j.meku_audio8fcb_actors_idb = n.id) where p.deleted != "1" and j.deleted != "1" and n.deleted != "1"')
-    .stream()
-    .pipe(consumer(function(row, done) {
-      tick()
-      if (!result[row.id]) result[row.id] = []
-      result[row.id].push(row.name)
-      done()
-    }, function() {
-      async.eachLimit(Object.keys(result), 5, function(key, cb) {
-        tick('*')
-        schema.Movie.update({ 'emeku-id': key }, { actors: result[key] }, cb)
-      }, callback)
-    }))
-}
+function metadata(callback) {
+  async.applyEachSeries([actors, directors, productionCompanies], callback)
 
-function directors(callback) {
-  var tick = progressMonitor()
-  var result = {}
-  conn.query('select p.id, CONCAT_WS(" ", TRIM(n.name), TRIM(n.surname)) as name from meku_audiovisualprograms p join meku_audiovku_directors_c j on (p.id = j.meku_audioc912rograms_ida) join meku_directors n on (j.meku_audioffd0rectors_idb = n.id) where p.deleted != "1" and j.deleted != "1" and n.deleted != "1"')
-    .stream()
-    .pipe(consumer(function(row, done) {
-      tick()
-      if (!result[row.id]) result[row.id] = []
-      result[row.id].push(row.name)
-      done()
-    }, function() {
-      async.eachLimit(Object.keys(result), 5, function(key, cb) {
-        tick('*')
-        schema.Movie.update({ 'emeku-id': key }, { directors: result[key] }, cb)
-      }, callback)
-    }))
-}
+  function actors(callback) {
+    console.log('\n> actors')
+    var q = 'select p.id, CONCAT_WS(" ", TRIM(n.name), TRIM(n.surname)) as name from meku_audiovisualprograms p join meku_audiov_meku_actors_c j on (p.id = j.meku_audio7d9crograms_ida) join meku_actors n on (j.meku_audio8fcb_actors_idb = n.id) where p.deleted != "1" and j.deleted != "1" and n.deleted != "1"'
+    batchUpdater(q, idToNameMapper, singleFieldUpdater('Movie', 'actors'), callback)
+  }
 
-function productionCompanies(callback) {
-  var tick = progressMonitor()
-  var result = {}
-  conn.query('select p.id, pc.name from meku_audiovisualprograms p join meku_audiovon_companies_c j on (p.id = j.meku_audioe15crograms_ida) join meku_production_companies pc on (j.meku_audio8018mpanies_idb = pc.id) where p.deleted != "1" and j.deleted != "1" and pc.deleted != "1"')
-    .stream()
-    .pipe(consumer(function(row, done) {
-      tick()
-      if (!result[row.id]) result[row.id] = []
-      result[row.id].push(row.name)
-      done()
-    }, function() {
-      async.eachLimit(Object.keys(result), 5, function(key, cb) {
-        tick('*')
-        schema.Movie.update({ 'emeku-id': key }, { 'production-companies': result[key] }, cb)
-      }, callback)
-    }))
+  function directors(callback) {
+    console.log('\n> directors')
+    var q = 'select p.id, CONCAT_WS(" ", TRIM(n.name), TRIM(n.surname)) as name from meku_audiovisualprograms p join meku_audiovku_directors_c j on (p.id = j.meku_audioc912rograms_ida) join meku_directors n on (j.meku_audioffd0rectors_idb = n.id) where p.deleted != "1" and j.deleted != "1" and n.deleted != "1"'
+    batchUpdater(q, idToNameMapper, singleFieldUpdater('Movie', 'directors'), callback)
+  }
+
+  function productionCompanies(callback) {
+    console.log('\n> productionCompanies')
+    var q = 'select p.id, pc.name from meku_audiovisualprograms p join meku_audiovon_companies_c j on (p.id = j.meku_audioe15crograms_ida) join meku_production_companies pc on (j.meku_audio8018mpanies_idb = pc.id) where p.deleted != "1" and j.deleted != "1" and pc.deleted != "1"'
+    batchUpdater(q, idToNameMapper, singleFieldUpdater('Movie', 'production-companies'), callback)
+  }
+
+  function idToNameMapper(row, result) {
+    if (!result[row.id]) result[row.id] = []
+    result[row.id].push(row.name)
+  }
 }
 
 function classifications(callback) {
-  // TODO: add c.status, c.no_items (== safe?), c.date_entered?, ...
+  // TODO: add c.no_items (== safe?)
   // TODO: verify that reg_date order is the valid ordering
 
   async.waterfall([base, criteria, mapUsers, mapBuyers, harmonize, save], callback)
@@ -288,78 +267,49 @@ function classifications(callback) {
 }
 
 function accounts(callback) {
-  async.applyEachSeries([accountBase, accountEmailAddresses], callback)
+  async.applyEachSeries([accountBase, accountEmailAddresses, providers, userBase, userEmails, demoUsers], callback)
 
   function accountBase(callback) {
-    conn.query('select id, name, customer_type from accounts where customer_type not like "%Location_of_providing%" and deleted != "1"')
-      .stream({ highWaterMark: 2000 })
-      .pipe(transformer(function(row) { return { 'emeku-id': row.id, name: trim(row.name), roles: optionListToArray(row.customer_type) } }))
-      .pipe(batcher(1000))
-      .pipe(batchInserter('Account', callback))
+    var q = 'select id, name, customer_type from accounts where customer_type not like "%Location_of_providing%" and deleted != "1"'
+    function onRow(row) { return { 'emeku-id': row.id, name: trim(row.name), roles: optionListToArray(row.customer_type) } }
+    batchInserter(q, onRow, 'Account', callback)
   }
 
   function accountEmailAddresses(callback) {
-    var tick = progressMonitor(100)
-    var result = {}
-    conn.query('select a.id, e.email_address, j.primary_address from accounts a join email_addr_bean_rel j on (j.bean_id = a.id) join email_addresses e on (j.email_address_id = e.id) where a.deleted != "1" and j.deleted != "1" and e.deleted != "1" and j.bean_module = "Accounts"')
-      .stream()
-      .pipe(consumer(function(row, done) {
-        tick()
-        if (!result[row.id]) result[row.id] = []
-        if (row.primary_address == '1') {
-          result[row.id].unshift(row.email_address)
-        } else {
-          result[row.id].push(row.email_address)
-        }
-        done()
-      }, function() {
-        async.eachLimit(Object.keys(result), 5, function(key, cb) {
-          tick('*')
-          schema.Account.update({ 'emeku-id': key }, { 'email-addresses': result[key] }, cb)
-        }, callback)
-      }))
-  }
-}
-
-function providers(callback) {
-  conn.query('select id, name from accounts where customer_type like "%Location_of_providing%" and deleted != "1"')
-    .stream({ highWaterMark: 2000 })
-    .pipe(transformer(function(row) { return { 'emeku-id': row.id, name: trim(row.name) } }))
-    .pipe(batcher(1000))
-    .pipe(batchInserter('Provider', callback))
-}
-
-function users(callback) {
-  async.applyEachSeries([base, emails], callback)
-
-  function base(callback) {
-    conn.query('select id, user_name, CONCAT_WS(" ", TRIM(first_name), TRIM(last_name)) as name, status from users')
-      .stream({ highWaterMark: 2000 })
-      .pipe(transformer(function(row) { return { 'emeku-id': row.id, username: row.user_name, name: row.name, active: row.status == 'Active' } }))
-      .pipe(batcher(1000))
-      .pipe(batchInserter('User', callback))
+    var q = 'select a.id, e.email_address, j.primary_address from accounts a join email_addr_bean_rel j on (j.bean_id = a.id) join email_addresses e on (j.email_address_id = e.id) where a.deleted != "1" and j.deleted != "1" and e.deleted != "1" and j.bean_module = "Accounts"'
+    batchUpdater(q, idToEmailMapper, singleFieldUpdater('Account', 'email-addresses'), callback)
   }
 
-  function emails(callback) {
-    var tick = progressMonitor(100)
-    var result = {}
-    conn.query('select u.id, e.email_address, j.primary_address from users u join email_addr_bean_rel j on (j.bean_id = u.id) join email_addresses e on (j.email_address_id = e.id) where u.deleted != "1" and j.deleted != "1" and e.deleted != "1" and j.bean_module = "Users"')
-      .stream()
-      .pipe(consumer(function(row, done) {
-        tick()
-        if (!result[row.id]) result[row.id] = []
-        if (row.primary_address == '1') {
-          result[row.id].unshift(row.email_address)
-        } else {
-          result[row.id].push(row.email_address)
-        }
-        done()
-      }, function() {
-        async.eachLimit(Object.keys(result), 5, function(key, cb) {
-          tick('*')
-          schema.User.update({ 'emeku-id': key }, { 'emails': result[key] }, cb)
-        }, callback)
-      }))
+  function providers(callback) {
+    var q = 'select id, name from accounts where customer_type like "%Location_of_providing%" and deleted != "1"'
+    function onRow(row) { return { 'emeku-id': row.id, name: trim(row.name) } }
+    batchInserter(q, onRow, 'Provider', callback)
+  }
+
+  function userBase(callback) {
+    var q = 'select id, user_name, CONCAT_WS(" ", TRIM(first_name), TRIM(last_name)) as name, status from users'
+    function onRow(row) { return { 'emeku-id': row.id, username: row.user_name, name: row.name, active: row.status == 'Active' } }
+    batchInserter(q, onRow, 'User', callback)
+  }
+
+  function userEmails(callback) {
+    var q = 'select u.id, e.email_address, j.primary_address from users u join email_addr_bean_rel j on (j.bean_id = u.id) join email_addresses e on (j.email_address_id = e.id) where u.deleted != "1" and j.deleted != "1" and e.deleted != "1" and j.bean_module = "Users"'
+    batchUpdater(q, idToEmailMapper, singleFieldUpdater('User', 'emails'), callback)
+  }
+
+  function demoUsers(callback) {
+    async.forEach(['root', 'kavi', 'user'], function(u, callback) {
+      new schema.User({ username:u, password:u, role:u, name:u, emails:[u+'@fake-meku.fi'] }).save(callback)
+    }, callback)
+  }
+
+  function idToEmailMapper(row, result) {
+    if (!result[row.id]) result[row.id] = []
+    if (row.primary_address == '1') {
+      result[row.id].unshift(row.email_address)
+    } else {
+      result[row.id].push(row.email_address)
+    }
   }
 }
 
@@ -417,16 +367,6 @@ function linkTvSeries(callback) {
   function trimPeriod(s) { return s && s.replace(/\.$/, '') || s }
 }
 
-function demoUsers(callback) {
-  var users = ['root', 'kavi', 'user']
-  schema.User.remove({ username: { $in: users } }, function(err) {
-    if (err) return callback(err)
-    async.forEach(users, function(u, callback) {
-      new schema.User({ username:u, password:u, role:u, name:u, emails:[u+'@fake-meku.fi'] }).save(callback)
-    }, callback)
-  })
-}
-
 function batcher(num) {
   var arr = []
   var tx = new stream.Transform({ objectMode: true })
@@ -446,24 +386,15 @@ function batcher(num) {
   return tx
 }
 
-function programMapper() {
-  return transformer(function(row) {
-    var obj = { 'emeku-id': row.id }
-    obj['program-type'] = legacyProgramTypes[row.program_type] || 0
-    if (row.publish_year && row.publish_year != 'undefined') obj.year = row.publish_year
-    if (row.year && row.year != 'undefined') obj.year = row.year
-    if (row.countries) obj.country = optionListToArray(row.countries)
-    if (row.description) obj.synopsis = trim(row.description)
-    var legacyGenre = optionListToArray(row.genre).map(function(g) { return legacyGenres[g] })
-      .concat(optionListToArray(row.tv_program_genre).map(function(g) { return legacyTvGenres[g] }))
-      .concat(optionListToArray(row.game_genre))
-    if (legacyGenre) obj['legacy-genre'] = _(legacyGenre).compact().uniq().value()
-    if (row.program_type == '11' || row.program_type == '08') obj.gameFormat = row.game_format
-    return obj
-  })
+function batchInserter(q, mapFn, modelName, callback) {
+  conn.query(q)
+    .stream({ highWaterMark: 2000 })
+    .pipe(transformer(mapFn))
+    .pipe(batcher(1000))
+    .pipe(inserter(modelName, callback))
 }
 
-function batchInserter(model, callback) {
+function inserter(model, callback) {
   var tick = progressMonitor(2)
   var onRows = function(rows, cb) {
     tick()
@@ -471,6 +402,23 @@ function batchInserter(model, callback) {
     schema[model].create.apply(schema[model], args)
   }
   return consumer(onRows, callback)
+}
+
+function batchUpdater(query, mapFn, updateFn, callback) {
+  var tick = progressMonitor()
+  var result = {}
+  conn.query(query)
+    .stream()
+    .pipe(consumer(function(row, done) {
+      tick()
+      mapFn(row, result)
+      done()
+    }, function() {
+      async.eachLimit(Object.keys(result), 5, function(key, cb) {
+        tick('*')
+        updateFn(key, result[key], cb)
+      }, callback)
+    }))
 }
 
 function progressMonitor(num) {
@@ -495,6 +443,12 @@ function consumer(onRow, callback) {
   s._write = function(row, enc, cb) { onRow(row, cb) }
   s.on('finish', callback)
   return s
+}
+
+function singleFieldUpdater(schemaName, fieldName) {
+  return function(key, obj, callback) {
+    schema[schemaName].update({ 'emeku-id': key }, utils.keyValue(fieldName, obj), callback)
+  }
 }
 
 function optionListToArray(string) {
@@ -534,26 +488,16 @@ function wipe(callback) {
   })
 }
 function wipeAccounts(callback) {
-  connectMongoose(function() { dropCollection('accounts', function() { callback() }) })
-}
-function wipeProviders(callback) {
-  connectMongoose(function() { dropCollection('providers', function() { callback() }) })
-}
-function wipeUsers(callback) {
-  connectMongoose(function() { dropCollection('users', function() { callback() }) })
+  connectMongoose(function() {
+    async.each(['accounts', 'providers', 'users'], dropCollection, callback)
+  })
 }
 
 function wipeNames(callback) {
   schema.Movie.update({}, { 'name': [], 'name-fi': [], 'name-sv': [], 'name-other': [] }, { multi:true }, callback)
 }
-function wipeActors(callback) {
-  schema.Movie.update({}, { 'actors': [] }, { multi:true }, callback)
-}
-function wipeDirectors(callback) {
-  schema.Movie.update({}, { 'directors': [] }, { multi:true }, callback)
-}
-function wipeProductionCompanies(callback) {
-  schema.Movie.update({}, { 'production-companies': [] }, { multi:true }, callback)
+function wipeMetadata(callback) {
+  schema.Movie.update({}, { actors: [], directors: [], 'production-companies': [] }, { multi:true }, callback)
 }
 function wipeClassifications(callback) {
   schema.Movie.update({}, { 'classifications': [] }, { multi:true }, callback)

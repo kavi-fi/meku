2142f5ea75345f47bb315a2ce23d45dd21b5a48d
Antti Holvikari <antti.holvikari@reaktor.fi>
2014-06-01 18:54:21 +0300

    Move enum stuff from sql-import to enums

--- server/xmlimport.js
+++ server/xmlimport.js
@@ -1,5 +1,6 @@
 var fs = require('fs'),
     xml = require('xml-object-stream'),
+    enums = require('../shared/enums.js'),
     _ = require('lodash'),
     moment = require('moment'),
     utils = require('../shared/utils')
@@ -30,16 +31,16 @@ var validateProgram = compose([
   optional('TUOTANTOYHTIO', 'legacy-production-companies'),
   map(and(requiredAttr('TYPE', 'type'), function(xml) {
     var type = xml.$.TYPE
-    if (_.has(legacyProgramTypes, type)) return ok({type: legacyProgramTypes[type]})
+    if (_.has(enums.legacyProgramTypes, type)) return ok({type: enums.legacyProgramTypes[type]})
     else return error("Virheellinen attribuutti: TYPE")
-  }), function(p) { return { 'program-type': legacyProgramTypes[p.type] }}),
+  }), function(p) { return { 'program-type': enums.legacyProgramTypes[p.type] }}),
   required('SYNOPSIS', 'synopsis'),
   optional('TUOTANTOKAUSI', 'season'),
   optional('OSA', 'episode'),
   function(xml) {
     return ok({
-      'legacy-genre': optionListToArray(xml.LAJIT).map(function(g) { return legacyGenres[g] })
-              .concat(optionListToArray(xml['TELEVISIO-OHJELMALAJIT']).map(function(g) { return legacyTvGenres[g] }))
+      'legacy-genre': optionListToArray(xml.LAJIT).map(function(g) { return enums.legacyGenres[g] })
+              .concat(optionListToArray(xml['TELEVISIO-OHJELMALAJIT']).map(function(g) { return enums.legacyTvGenres[g] }))
               .concat(optionListToArray(xml.PELINLAJIT))
     })
   },
@@ -163,81 +164,3 @@ function optionListToArray(field) {
   return _(arr).compact().uniq().value()
 }
 
-var legacyGenres = {
-  '1': 'Fiktio',
-  '2': 'Animaatio',
-  '3': 'Viihde',
-  '4': 'Seksi / Erotiikka',
-  '5': 'Dokumentti',
-  '6': 'Lasten ohjelmat',
-  '9': 'Muut',
-  '1a': 'Romantiikka',
-  '1b': 'Draama',
-  '1c': 'Historiallinen näytelmä',
-  '1d': 'Trilleri',
-  '1e': 'Seikkailu',
-  '1f': 'Western',
-  '1g': 'Sota',
-  '1h': 'Kauhu',
-  '1j': 'Tieteisseikkailu',
-  '1k': 'Toimintaelokuva',
-  '1m': 'Fantasia',
-  '1n': 'Komedia ja farssi',
-  '3c': 'Musiikki',
-  '5a': 'Opetus',
-  '5b': 'Urheilu',
-  '5c': 'Matkailu',
-  '5d': 'Henkilödokumentti',
-  '5e': 'Luontodokumentti',
-  '6a': 'Lasten animaatio',
-  '6b': 'Lasten fiktio',
-  '6c': 'Muu lastenelokuva',
-  '9a': 'Mainonta',
-  '9c': 'Tietoisku',
-  '9d': 'Kokeiluelokuva',
-  '9e': 'Videotaide',
-  '7a': 'Trailer',
-  '7b': 'Extra'
-}
-var legacyTvGenres = {
-  '2': 'Ajankohtaisohjelmat',
-  '3': 'Asiaohjelmat/kulttuuri/lifestyle',
-  '4': 'Urheilu',
-  '5': 'Kotimainen fiktio',
-  '6': 'Ulkomainen fiktio',
-  '7': 'Elokuvat',
-  '8': 'Lasten ohjelmat',
-  '9': 'Opetus- ja tiedeohjelmat',
-  '10': 'Viihde/kevyt musiikki/reality',
-  '11': 'Muu musiikki',
-  '12': 'Muut ohjelmat',
-  '1.': 'Uutiset',
-  '1.1': 'Vakiouutiset',
-  '1.2': 'Muut uutislähetykset',
-  '3.1': 'Asiaohjelmat/lifestyle',
-  '3.2': 'Kulttuuriohjelmat',
-  '4.1': 'Urheilu-uutiset',
-  '4.2': 'Muu urheilu',
-  '7.1': 'Kotimaiset elokuvat',
-  '7.2': 'Ulkomaiset elokuvat',
-  '12.1': 'Ostos-TV',
-  '12.2': 'WWW (peliohjelmat, chatit)',
-  '10.1': 'Kotimainen viihde/kevyt musiikki/reality',
-  '10.2': 'Ulkomainen viihde/kevyt musiikki/reality',
-  '13': 'Populaarikulttuuri'
-}
-
-var legacyProgramTypes = {
-  '01': 1,       //'Kotimainen elokuva' -> movie
-  '02': 1,       //'Ulkomainen elokuva' -> movie
-  '02b': 0,    // 'TESTI' -> unknown
-  '03': 3,       //'TV-sarjan jakso' -> tv
-  '04': 3,       // 'Muu tv-ohjelma' -> tv
-  '05': 2,       // 'TV-sarjan nimi' -> series
-  '06': 5,       // 'Traileri' -> trailer
-  '07': 4,       // 'Extra' -> extra
-  '08': 6,       // 'Peli' -> game
-  '10': 0,      // 'Yhteistuotanto' -> unknown
-  '11': 7,      // 'PEGI hyväksytty peli' -> game
-  '12': 0       // 'Muu kuvaohjelma' -> unknown
-}
--- shared/enums.js
+++ shared/enums.js
@@ -282,6 +282,86 @@ enums.classificationCriteria = [
   { id:37, category: 'drugs',    age: '12', title: "Huumeiden ei-hallitsevaa käyttöä / alaikäisten alkoholin käyttöä", description: "Tällä tarkoitetaan huumeiden viitteellistä tai vähäistä käyttöä tai alaikäisten korostettua, viihteellistä tai ongelmatonta alkoholin käyttöä." }
 ]
 
+enums.legacyGenres = {
+  '1': 'Fiktio',
+  '2': 'Animaatio',
+  '3': 'Viihde',
+  '4': 'Seksi / Erotiikka',
+  '5': 'Dokumentti',
+  '6': 'Lasten ohjelmat',
+  '9': 'Muut',
+  '1a': 'Romantiikka',
+  '1b': 'Draama',
+  '1c': 'Historiallinen näytelmä',
+  '1d': 'Trilleri',
+  '1e': 'Seikkailu',
+  '1f': 'Western',
+  '1g': 'Sota',
+  '1h': 'Kauhu',
+  '1j': 'Tieteisseikkailu',
+  '1k': 'Toimintaelokuva',
+  '1m': 'Fantasia',
+  '1n': 'Komedia ja farssi',
+  '3c': 'Musiikki',
+  '5a': 'Opetus',
+  '5b': 'Urheilu',
+  '5c': 'Matkailu',
+  '5d': 'Henkilödokumentti',
+  '5e': 'Luontodokumentti',
+  '6a': 'Lasten animaatio',
+  '6b': 'Lasten fiktio',
+  '6c': 'Muu lastenelokuva',
+  '9a': 'Mainonta',
+  '9c': 'Tietoisku',
+  '9d': 'Kokeiluelokuva',
+  '9e': 'Videotaide',
+  '7a': 'Trailer',
+  '7b': 'Extra'
+}
+
+enums.legacyTvGenres = {
+  '2': 'Ajankohtaisohjelmat',
+  '3': 'Asiaohjelmat/kulttuuri/lifestyle',
+  '4': 'Urheilu',
+  '5': 'Kotimainen fiktio',
+  '6': 'Ulkomainen fiktio',
+  '7': 'Elokuvat',
+  '8': 'Lasten ohjelmat',
+  '9': 'Opetus- ja tiedeohjelmat',
+  '10': 'Viihde/kevyt musiikki/reality',
+  '11': 'Muu musiikki',
+  '12': 'Muut ohjelmat',
+  '1.': 'Uutiset',
+  '1.1': 'Vakiouutiset',
+  '1.2': 'Muut uutislähetykset',
+  '3.1': 'Asiaohjelmat/lifestyle',
+  '3.2': 'Kulttuuriohjelmat',
+  '4.1': 'Urheilu-uutiset',
+  '4.2': 'Muu urheilu',
+  '7.1': 'Kotimaiset elokuvat',
+  '7.2': 'Ulkomaiset elokuvat',
+  '12.1': 'Ostos-TV',
+  '12.2': 'WWW (peliohjelmat, chatit)',
+  '10.1': 'Kotimainen viihde/kevyt musiikki/reality',
+  '10.2': 'Ulkomainen viihde/kevyt musiikki/reality',
+  '13': 'Populaarikulttuuri'
+}
+
+enums.legacyProgramTypes = {
+  '01': 1,       //'Kotimainen elokuva' -> movie
+  '02': 1,       //'Ulkomainen elokuva' -> movie
+  '02b': 0,    // 'TESTI' -> unknown
+  '03': 3,       //'TV-sarjan jakso' -> tv
+  '04': 3,       // 'Muu tv-ohjelma' -> tv
+  '05': 2,       // 'TV-sarjan nimi' -> series
+  '06': 5,       // 'Traileri' -> trailer
+  '07': 4,       // 'Extra' -> extra
+  '08': 6,       // 'Peli' -> game
+  '10': 0,      // 'Yhteistuotanto' -> unknown
+  '11': 7,      // 'PEGI hyväksytty peli' -> game
+  '12': 0       // 'Muu kuvaohjelma' -> unknown
+}
+
 if (typeof module !== 'undefined' && module.exports) {
   module.exports = enums
 }

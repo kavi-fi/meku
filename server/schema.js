2dda83a14bc9fd7b5fff39d39a27fe718b37e933
Mikko Koponen <mikko.koponen@reaktor.fi>
2014-06-13 23:32:30 +0300

    Hide reclassification data in public UI

--- server/schema.js
+++ server/schema.js
@@ -81,6 +81,7 @@ Program.publicFields = {
   emekuId:0, customersId:0, allNames:0,
   'classifications.emekuId':0, 'classifications.author':0, 'classifications.format': 0,
   'classifications.billing': 0, 'classifications.buyer': 0, 'classifications.registrationEmailAddresses':0,
+  'classifications.authorOrganization': 0, 'classifications.reason': 0,
   'classifications.comments':0, 'classifications.criteriaComments':0
 }
 

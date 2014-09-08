be261347dc6adf300effad3878539aec4dbe0ee9
Antti Holvikari <antti.holvikari@reaktor.fi>
2014-09-08 14:46:09 +0300

    Add bash script to run the whole import

---
+++ scripts/import.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <path to userlist .xlsx>"
+  exit
+fi
+
+node scripts/sql-import.js wipe
+node scripts/sql-import.js sequences
+node scripts/sql-import.js accounts
+echo "> Adding emails for users from $1"
+node scripts/map-user-emails.js logUpdates $1 | mongo meku
+node scripts/sql-import.js programs
+node scripts/sql-import.js names
+node scripts/sql-import.js metadata classifications markTrainingProgramsDeleted \
+  markUnclassifiedProgramsDeleted linkTvSeries linkCustomersIds metadataIndex nameIndex
+node scripts/create-demo-data.js

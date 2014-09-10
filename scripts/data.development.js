efe3dcf921d7f27b26b03699baace8201e0f2e41
Mikko Koponen <mikko.koponen@reaktor.fi>
2014-09-10 18:41:00 +0300

    Fix demo users to name uppercase USERNAMEs

--- scripts/data.development.js
+++ scripts/data.development.js
@@ -1,12 +1,12 @@
 exports.users = [
-  {username:'root', active: true, password:'root', role:'root', name:'root', emails:['root@fake-meku.fi'] },
-  {username:'kavi', active: true, password:'kavi', role:'kavi', name:'kavi', emails:['kavi@fake-meku.fi'] },
-  {username:'user', active: true, password:'user', role:'user', name:'user', emails:['user@fake-meku.fi'] }
+  {username:'ROOT', active: true, password:'root', role:'root', name:'root', emails:['root@fake-meku.fi'] },
+  {username:'KAVI', active: true, password:'kavi', role:'kavi', name:'kavi', emails:['kavi@fake-meku.fi'] },
+  {username:'USER', active: true, password:'user', role:'user', name:'user', emails:['user@fake-meku.fi'] }
 ]
 
 exports.accounts = [
-  {name: "DEMO tilaaja 1", roles: ['Subscriber'], emailAddresses: [], users: ['root', 'kavi', 'user'], yTunnus: 'DEMO1' },
-  {name: "DEMO tilaaja 2", roles: ['Subscriber'], emailAddresses: [], users: ['root', 'kavi', 'user'], yTunnus: 'DEMO2' },
-  {name: "DEMO tilaaja 3", roles: ['Subscriber', 'Classifier'], emailAddresses: [], users: ['root', 'kavi', 'user'], yTunnus: 'DEMO3' }
+  {name: "DEMO tilaaja 1", roles: ['Subscriber'], emailAddresses: [], users: ['ROOT', 'KAVI', 'USER'], yTunnus: 'DEMO1' },
+  {name: "DEMO tilaaja 2", roles: ['Subscriber'], emailAddresses: [], users: ['ROOT', 'KAVI', 'USER'], yTunnus: 'DEMO2' },
+  {name: "DEMO tilaaja 3", roles: ['Subscriber', 'Classifier'], emailAddresses: [], users: ['ROOT', 'KAVI', 'USER'], yTunnus: 'DEMO3' }
 ]
 

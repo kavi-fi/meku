b47bcc612256013debd7f7c2c4378b820e85657c
Niko Tobiasson <niko.tobiasson@reaktor.com>
2016-10-03 15:17:35 +0300

    New columns added to excel report

--- server/program-export.js
+++ server/program-export.js
@@ -1,8 +1,10 @@
-var _ = require('lodash')
-var moment = require('moment')
-var excelWriter = require('./excel-writer')
-var fs = require('fs')
-var enums = require('../shared/enums')
+const _ = require('lodash')
+const moment = require('moment')
+const excelWriter = require('./excel-writer')
+const fs = require('fs')
+const enums = require('../shared/enums')
+const utils = require('../shared/utils')
+
 
 var dateFormat = 'DD.MM.YYYY'
 
@@ -14,22 +16,27 @@ exports.constructProgramExportData = function constructProgramExportData(docs, s
 function createData(docs, showClassificationAuthor) {
 
   var tmpXlsxFile = "kavi_luokittelut.xlsx"
-  var columns = ["Alkuperäinen nimi", "Suomenkielinen nimi", "Luokittelun tilaaja", "Luokittelija", "Ikäraja", "Varoitukset", "Ohjelman tyyppi", "Maa", "Valmistumisvuosi", "Ohjaaja", "Tuotantoyhtiö", "Synopsis"]
-  columnWidths = [{"wch":40},{"wch":40},{"wch":40},{"wch":40},{"wch":10},{"wch":20},{"wch":20},{"wch":10},{"wch":15},{"wch":20},{"wch":20},{"wch":40}]
+  var columns = ["Alkuperäinen nimi", "Suomenkielinen nimi", "Jakso", "Jakson nimi", "Rekisteröintipäivä", "Kesto", "Luokittelun tilaaja", "Luokittelija", "Uudelleenluokittelija", "Ikäraja", "Perustelu", "Varoitukset", "Ohjelman tyyppi", "Maa", "Valmistumisvuosi", "Ohjaaja", "Tuotantoyhtiö", "Synopsis"]
+  columnWidths = [{"wch":40},{"wch":40},{"wch":10},{"wch":40},{"wch":15},{"wch":10},{"wch":40},{"wch":25},{"wch":25},{"wch":10},{"wch":40},{"wch":20},{"wch":20},{"wch":10},{"wch":15},{"wch":20},{"wch":20},{"wch":40}]
 
 
   var xlsData = []
   xlsData.push(columns)
   _.forEach(docs, function(doc){
+    doc.originalName = doc.programType === 3 ? doc.series.name : doc.name.join(', ')
     doc.format = doc.classifications[0] === undefined ? "" : doc.classifications[0].format
     doc.duration = doc.classifications[0] === undefined ? "" : doc.classifications[0].duration
     doc.classificationBuyer = doc.classifications[0] === undefined ? "" : doc.classifications[0].buyer === undefined ? "" : doc.classifications[0].buyer.name
     doc.classificationAuthor = doc.classifications[0] === undefined ? "" : doc.classifications[0].author === undefined ? "" :  showClassificationAuthor ? doc.classifications[0].author.name : ""
     doc.agelimit = doc.classifications[0] === undefined ? "" : doc.classifications[0].agelimit === undefined ? "" : doc.classifications[0].agelimit
     doc.warnings = doc.classifications[0] === undefined ? "" : doc.classifications[0].warnings === undefined ? "" : doc.classifications[0].warnings.join(', ')
+    doc.criteriaComments = doc.classifications[0] === undefined ? "" : doc.classifications[0].isReclassification ? _.values(doc.classifications[0].criteriaComments).join(', ') : ""
+    doc.episodeCode = doc.programType === 3 ? utils.seasonEpisodeCode(doc) : ""
+    doc.episodeName = doc.programType === 3 ? doc.name.join(', ') : ""
+    doc.reclassifier = doc.classifications[0] === undefined ? "" : doc.classifications[0].isReclassification ? doc.classifications[0].authorOrganization === undefined ? "" : enums.authorOrganization[doc.classifications[0].authorOrganization] : ""
 
 
-    xlsData.push([doc.name.join(', '), doc.nameFi.join(', '), doc.classificationBuyer, doc.classificationAuthor, doc.agelimit, doc.warnings, enums.programType[doc.programType].fi, doc.country.join(', '), doc.year, doc.directors.join(', '), doc.productionCompanies.join(', '), doc.synopsis])
+    xlsData.push([doc.originalName, doc.nameFi.join(', '), doc.episodeCode, doc.episodeName, moment(doc.registrationDate).format(dateFormat), doc.duration, doc.classificationBuyer, doc.classificationAuthor, doc.reclassifier, doc.agelimit, doc.criteriaComments, doc.warnings, enums.programType[doc.programType].fi, doc.country.join(', '), doc.year, doc.directors.join(', '), doc.productionCompanies.join(', '), doc.synopsis])
   })
   
   excelWriter.write(tmpXlsxFile, xlsData, columnWidths)

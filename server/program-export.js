0764c44da63c2a8078ca784398a176c1a05e1055
Nuutti Rintala <nuutti.rintala@reaktor.fi>
2017-01-04 10:51:03 +0200

    Remove consecutive blank lines

--- server/program-export.js
+++ server/program-export.js
@@ -5,20 +5,16 @@ const fs = require('fs')
 const enums = require('../shared/enums')
 const utils = require('../shared/utils')
 
-
 var dateFormat = 'DD.MM.YYYY'
 
 exports.constructProgramExportData = function constructProgramExportData(docs, showClassificationAuthor) {
   return createData(docs, showClassificationAuthor)
 }
 
-
 function createData(docs, showClassificationAuthor) {
-
   var tmpXlsxFile = "kavi_luokittelut.xlsx"
   var columns = ["Alkuperäinen nimi", "Suomenkielinen nimi", "Jakso", "Jakson nimi", "Rekisteröintipäivä", "Kesto", "Luokittelun tilaaja", "Luokittelija", "Uudelleenluokittelija", "Ikäraja", "Perustelu", "Varoitukset", "Ohjelman tyyppi", "Maa", "Valmistumisvuosi", "Ohjaaja", "Tuotantoyhtiö", "Synopsis"]
   columnWidths = [{"wch":40},{"wch":40},{"wch":10},{"wch":40},{"wch":15},{"wch":10},{"wch":40},{"wch":25},{"wch":25},{"wch":10},{"wch":40},{"wch":20},{"wch":20},{"wch":10},{"wch":15},{"wch":20},{"wch":20},{"wch":40}]
-
 
   var xlsData = []
   xlsData.push(columns)
@@ -34,7 +30,6 @@ function createData(docs, showClassificationAuthor) {
     doc.episodeCode = doc.programType === 3 ? utils.seasonEpisodeCode(doc) : ""
     doc.episodeName = doc.programType === 3 ? doc.name.join(', ') : ""
     doc.reclassifier = doc.classifications[0] === undefined ? "" : doc.classifications[0].isReclassification ? doc.classifications[0].authorOrganization === undefined ? "" : enums.authorOrganization[doc.classifications[0].authorOrganization] : ""
-
 
     xlsData.push([doc.originalName, doc.nameFi.join(', '), doc.episodeCode, doc.episodeName, moment(doc.classifications[0].registrationDate).format(dateFormat), doc.duration, doc.classificationBuyer, doc.classificationAuthor, doc.reclassifier, doc.agelimit, doc.criteriaComments, doc.warnings, enums.programType[doc.programType].fi, doc.country.join(', '), doc.year, doc.directors.join(', '), doc.productionCompanies.join(', '), doc.synopsis])
   })

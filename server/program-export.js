61484366a210e78d3e4fe0fa81ebea628d8c4054
Petri Ämmälä <petri.ammala@reaktor.com>
2017-02-27 11:35:23 +0200

    Fix export to work with imcomplete data

--- server/program-export.js
+++ server/program-export.js
@@ -18,21 +18,28 @@ function createData(docs, showClassificationAuthor) {
 
   var xlsData = []
   xlsData.push(columns)
-  _.forEach(docs, function(doc){
-    doc.originalName = doc.programType === 3 ? doc.series.name : doc.name.join(', ')
-    doc.format = doc.classifications[0] === undefined ? "" : doc.classifications[0].format
-    doc.duration = doc.classifications[0] === undefined || enums.util.isGameType(doc) ? "" : doc.classifications[0].duration
-    doc.classificationBuyer = doc.classifications[0] === undefined ? "" : doc.classifications[0].buyer === undefined ? "" : doc.classifications[0].buyer.name
-    doc.classificationAuthor = doc.classifications[0] === undefined ? "" : doc.classifications[0].author === undefined ? "" :  showClassificationAuthor ? doc.classifications[0].author.name : ""
-    doc.agelimit = doc.classifications[0] === undefined ? "" : doc.classifications[0].agelimit === undefined ? "" : doc.classifications[0].agelimit
-    doc.warnings = doc.classifications[0] === undefined ? "" : doc.classifications[0].warnings === undefined ? "" : doc.classifications[0].warnings.join(', ')
-    doc.criteriaComments = doc.classifications[0] === undefined ? "" : doc.classifications[0].isReclassification ? _.values(doc.classifications[0].criteriaComments).join(', ') : ""
-    doc.criteria = doc.classifications[0] === undefined ? "" : doc.classifications[0].criteria === undefined ? "" : doc.classifications[0].criteria
-    doc.episodeCode = doc.programType === 3 ? utils.seasonEpisodeCode(doc) : ""
-    doc.episodeName = doc.programType === 3 ? doc.name.join(', ') : ""
-    doc.reclassifier = doc.classifications[0] === undefined ? "" : doc.classifications[0].isReclassification ? doc.classifications[0].authorOrganization === undefined ? "" : enums.authorOrganization[doc.classifications[0].authorOrganization] : ""
+  _.forEach(docs, function (doc) {
+    var name = listToString(doc.name)
+    var classification = doc.classification && doc.classification[0] ? doc.classification[0] : {}
+    doc.originalName = enums.util.isTvEpisode(doc) ? (doc.series || {}).name : name
+    doc.format = classification.format
+    doc.duration = enums.util.isGameType(doc) ? "" :classification.duration
+    doc.classificationBuyer = classification.buyer ? doc.classifications[0].buyer.name : ""
+    doc.classificationAuthor = classification.author && showClassificationAuthor ? doc.classifications[0].author.name : ""
+    doc.agelimit = classification.agelimit
+    doc.warnings = classification.warnings ? doc.classifications[0].warnings.join(', ') : ""
+    doc.criteriaComments = classification.isReclassification ? _.values(classification.criteriaComments).listToString(', ') : ""
+    doc.criteria = classification.criteria
+    doc.episodeCode = enums.util.isTvEpisode(doc) ? utils.seasonEpisodeCode(doc) : ""
+    doc.episodeName = enums.util.isTvEpisode(doc) ? name : ""
+    doc.reclassifier = classification.isReclassification && classification.authorOrganization ? enums.authorOrganization[classification.authorOrganization] : ""
+    doc.registrationDate = classification.registrationDate ? moment(classification.registrationDate).format(dateFormat) : ""
 
-    xlsData.push([doc.originalName, doc.nameFi.join(', '), doc.episodeCode, doc.episodeName, moment(doc.classifications[0].registrationDate).format(dateFormat), doc.duration, doc.classificationBuyer, doc.classificationAuthor, doc.reclassifier, doc.agelimit, doc.criteria, doc.criteriaComments, doc.warnings, enums.programType[doc.programType].fi, doc.country.join(', '), doc.year, doc.directors.join(', '), doc.productionCompanies.join(', '), doc.synopsis])
+    xlsData.push([doc.originalName, listToString(doc.nameFi), doc.episodeCode, doc.episodeName, doc.registrationDate, doc.duration, doc.classificationBuyer, doc.classificationAuthor, doc.reclassifier, doc.agelimit, doc.criteria, doc.criteriaComments, doc.warnings, enums.programType[doc.programType].fi, listToString(doc.country), doc.year, listToString(doc.directors), listToString(doc.productionCompanies), doc.synopsis])
+
+    function listToString(list) {
+      return (list || []).join(', ')
+    }
   })
 
   excelWriter.write(tmpXlsxFile, xlsData, columnWidths)

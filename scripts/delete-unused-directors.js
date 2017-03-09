36c3044414669492b14b14bf7ba45bdc6014f92d
Petri Ämmälä <petri.ammala@reaktor.com>
2017-03-09 10:47:11 +0200

    More efficient script to remove unused directors

--- scripts/delete-unused-directors.js
+++ scripts/delete-unused-directors.js
@@ -1,4 +1,6 @@
 // Usage: [MONGOHQ_URL=...] node ./ldelete-unused-directors.js
+//
+// It takes about 100 seconds to run the script remotely over 200000 programs (85000 directors, 25000 unique)
 
 var env = require('../server/env').get()
 var mongoose = require('mongoose')
@@ -6,7 +8,6 @@ var schema = require('../server/schema')
 var Program = schema.Program
 var Director = schema.Director
 var _ = require('lodash')
-var count = 0
 var suspicious = 0
 var deleted = 0
 
@@ -16,43 +17,47 @@ function isSuspicious(name) {
 
 mongoose.connect(process.env.MONGOHQ_URL || env.mongoUrl)
 
-Director.count({}, function (err, total) {
-  if (err) return console.error(err)
+var directors = []
+var programStream = Program.find({deleted: {$ne: true}}, {directors: 1}).stream()
 
-  var stream = Director.find({}).stream()
+programStream.on('data', function (data) {
+  directors = directors.concat(data.directors)
+})
 
-  stream.on('data', function (director) {
-    stream.pause()
-    Program.find({deleted: {$ne: true}, directors: {$in: [director.name]}}, function (err, data) {
-      if (err) console.error(err)
-      if (++count % 100 == 0) console.info('Processed ' + count + '/' + total)
-      if (data.length == 0) {
-        Director.remove({name: director.name}, function (err) {
-          if (err) console.error(err)
-          else {
-            console.info('Removed unused: ' + director.name)
-            deleted++
-          }
-          stream.resume()
-        })
-      } else if (isSuspicious(director.name)) {
-        console.info('Suspicious (not removed): ' + director.name)
-        suspicious++
-        stream.resume()
-      } else {
-        stream.resume()
-      }
-    })
+programStream.on('error', function (err) {
+  console.error('Error: ' + err)
+})
+
+programStream.on('close', function () {
+  console.info('Directors on programs', directors.length)
+  directors = _.uniq(directors)
+  console.info('Unique directors on programs', directors.length)
+  var directorStream = Director.find({}, {_id:1,name:1}).stream()
+
+  directorStream.on('data', function (director) {
+    if (!_.contains(directors, director.name)) {
+      directorStream.pause()
+      Director.remove({name: director.name}, function (err) {
+        if (err) console.error(err)
+        else {
+          console.info('Removed unused: ' + director.name)
+          deleted++
+        }
+        directorStream.resume()
+      })
+    } else if (isSuspicious(director.name)) {
+      console.info('Suspicious (not removed): "' + director.name + '"')
+      suspicious++
+    }
   })
 
-  stream.on('error', function (err) {
+  directorStream.on('error', function (err) {
     console.error('Error: ' + err)
   })
 
-  stream.on('close', function () {
+  directorStream.on('close', function () {
     console.info('Deleted: ' + deleted)
     console.info('Suspicious: ' + suspicious)
     mongoose.disconnect()
   })
 })
-

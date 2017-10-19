aa3f3a8908615dc890f3015e916ae61e163e0c24
Petri Ämmälä <petri.ammala@reaktor.com>
2017-10-19 10:50:41 +0300

    Revert "Fix not loading recent classifications or drafts unless having at least kavi role"
    There is also role user that should get recent classification and drafts.
    
    This reverts commit 9cbd1cb6bb6e967a2952849f0c97fce06e77af1b.

--- client/js/search.js
+++ client/js/search.js
@@ -188,7 +188,6 @@ function internalSearchPage() {
   })
 
   function loadDrafts() {
-    if (!hasRole('kavi')) return loginPage().show()
     $.get('/programs/drafts', function(drafts) {
       $drafts.find('.draft').remove()
       drafts.forEach(function(draft) {
@@ -204,7 +203,6 @@ function internalSearchPage() {
 
   function loadRecent() {
     $recent.find('.program-box').remove()
-    if (!hasRole('kavi')) return
     $.get('/programs/recent', function(recents) {
       $recent.hide()
       $recent.find('.result').remove()

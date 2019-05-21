f2935f0843e4fe65f4fe77758b10bbecbddb5412
Niko Tobiasson <niko.tobiasson@reaktor.com>
2019-05-21 15:31:38 +0300

    Added improvements to the script used to fix duration format.

--- migrations/fix-duration-time-format.js
+++ migrations/fix-duration-time-format.js
@@ -9,12 +9,21 @@ var stream = Program.find({}).stream()
 
 stream.on('data', function (program) {
   const fixedClassifications = _.map(program.classifications, (classification) => {
-    if (classification.duration && classification.duration.length < 6){
-      console.log(`SequenceId: ${program.sequenceId}  Duration: ${classification.duration}`)
-      return classification
-    }
-    if (classification.duration && (classification.duration.length >= 6 && classification.duration.length < 8)){
-      var m = /(?:(\d+)?:)(\d+):(\d+)$/.exec(classification.duration)
+    if (classification.duration && classification.duration.trim().length < 6){
+      var match = /^(\d+):(\d+)$/.exec(classification.duration.trim())
+      if (match) {
+        const hours = '00'
+        const minutes = match[1] && match[1].length == 2 ? match[1] : match[1] ? match[1].padStart(2, '0') : '00'
+        const seconds = match[2] && match[2].length == 2 ? match[2] : match[2] ? match[2].padStart(2, '0') : '00'
+
+        const fixedTime = `${hours}:${minutes}:${seconds}`
+        return _.extend(classification, { duration: fixedTime })
+      } else {
+        console.log(`Name: ${_.join(program.name, ', ')}, SequenceId: ${program.sequenceId}, Duration: ${classification.duration}`)
+        return classification
+      }
+    } else if (classification.duration && (classification.duration.trim().length >= 6 && classification.duration.trim().length < 8)){
+      var m = /(?:(\d+)?:)(\d+):(\d+)$/.exec(classification.duration.trim())
       if (m){
         const hours = m[1] && m[1].length == 2 ? m[1] : m[1] ? m[1].padStart(2, '0') : '00'
         const minutes = m[2] && m[2].length == 2 ? m[2] : m[2] ? m[2].padStart(2, '0') : '00'
@@ -23,18 +32,21 @@ stream.on('data', function (program) {
         const fixedTime = `${hours}:${minutes}:${seconds}`
         return _.extend(classification, {duration: fixedTime})
       } else {
-        console.log(`Name: ${_.join(program.name, ', ')} SequenceId: ${program.sequenceId}  Duration: ${classification.duration}`)
+        console.log(`Name: ${_.join(program.name, ', ')}, SequenceId: ${program.sequenceId}, Duration: ${classification.duration}`)
         return classification
       }
+    } else if(classification.duration && classification.duration.trim().length > 8){
+        console.log(`Name: ${_.join(program.name, ', ')}, SequenceId: ${program.sequenceId}, Duration: ${classification.duration}`)
+        return classification
     }
     return classification
   })
 
   const updatedProgram = _.extend(program, {classifications: fixedClassifications})
-  updatedProgram.save(function (err) {
+  /*updatedProgram.save(function (err) {
     if (err) console.error(err)
     stream.resume()
-  })
+  })*/
 })
 stream.on('error', function (err) {
    console.error('Error: ' + err)

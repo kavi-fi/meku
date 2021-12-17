5ac9d518ed18a5fbe3e30c11d6cdeb576630eac4
Nuutti Rintala <nuutti.rintala@reaktor.fi>
2021-12-17 15:02:17 +0200

    Fix tests to work when run consecutively

--- cypress/fixtures/accounts.js
+++ cypress/fixtures/accounts.js
@@ -3,5 +3,6 @@ const users = require('./users')
 module.exports = [
   {name: "DEMO tilaaja 1", roles: ['Subscriber'], emailAddresses: ['demo.1@email.org'], users: [], yTunnus: 'DEMO1', address: {street: '', zip: '00000', city: 'Helsinki'}},
   {name: "DEMO tilaaja 2", roles: ['Subscriber'], emailAddresses: ['demo.2@email.org'], users: users, yTunnus: 'DEMO2', address: {street: '', zip: '00000', city: 'Helsinki'}},
-  {name: "DEMO tilaaja 3", roles: ['Subscriber', 'Classifier'], emailAddresses: ['demo.3@email.org'], users: users, yTunnus: 'DEMO3', address: {street: '', zip: '00000', city: 'Helsinki'}, apiToken: 'clientToken'}
+  {name: "DEMO tilaaja 3", roles: ['Subscriber', 'Classifier'], emailAddresses: ['demo.3@email.org'], users: users, yTunnus: 'DEMO3', address: {street: '', zip: '00000', city: 'Helsinki'}, apiToken: 'clientToken'},
+  {name: "DEMO tilaaja 4", roles: ['Subscriber', 'Classifier'], emailAddresses: ['demo.4@email.org'], users: users, yTunnus: 'DEMO4', address: {street: '', zip: '00000', city: 'Helsinki'}, apiToken: 'clientToken'}
 ]
--- cypress/integration/subscribers_spec.js
+++ cypress/integration/subscribers_spec.js
@@ -6,28 +6,29 @@ describe('Subscribers', () => {
 
   it('search result total is shown', () => {
     cy.visit("#tilaajat/")
-    cy.getDataCy('subcriber-result-count').should('have.text', '3')
+    cy.getDataCy('subcriber-result-count').should('have.text', '4')
   })
 
   it('should inactivate subscriber', () => {
     cy.visit("#tilaajat/")
-    cy.getDataCy('subcriber-result-count').should('have.text', '3')
-    cy.getDataCy('result').eq(2).click()
+    cy.getDataCy('subcriber-result-count').should('have.text', '4')
+    //remove DEMO tilaaja 4 account
+    cy.getDataCy('result').eq(3).click()
     cy.getDataCy('inactivate-subscriber-button:first').click()
     cy.getDataCy('inactivate-subscriber-dialog').should('be.visible')
     cy.getDataCy('inactivate-subscriber-dialog:last buttons confirm-inactivate').click()
-    cy.getDataCy('result').eq(2).should('have.class', 'inactive')
-    cy.getDataCy('subcriber-result-count').should('have.text', '2')
+    cy.getDataCy('result').eq(3).should('have.class', 'inactive')
+    cy.getDataCy('subcriber-result-count').should('have.text', '3')
   })
 
   it('inactivated subscribers should not show in classification buyer and billing options', () => {
     cy.visit("#haku/")
     cy.get('a[href="#haku"].active').should('be.visible')
     cy.getDataCy('search-page controls create').click()
-    cy.select2one('#classification-page .select2-container.x-buyer', 'DEMO tilaaja 3')
+    cy.select2one('#classification-page .select2-container.x-buyer', 'DEMO tilaaja 4')
     cy.get('#classification-page .select2-container.x-buyer').should('not.contain', 'DEMO tilaaja 3')
     cy.closeSelect2()
-    cy.select2one('#classification-page .select2-container.x-billing', 'DEMO tilaaja 3')
+    cy.select2one('#classification-page .select2-container.x-billing', 'DEMO tilaaja 4')
     cy.get('#classification-page .select2-container.x-billing').should('not.contain', 'DEMO tilaaja 3')
   })
 })

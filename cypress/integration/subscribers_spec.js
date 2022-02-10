8c75a076ab7d1711fff3c390a48277581c5f670b
Nuutti Rintala <nuutti.rintala@reaktor.fi>
2022-02-10 11:26:09 +0200

    Fix spelling error

--- cypress/integration/subscribers_spec.js
+++ cypress/integration/subscribers_spec.js
@@ -6,19 +6,19 @@ describe('Subscribers', () => {
 
   it('search result total is shown', () => {
     cy.visit("#tilaajat/")
-    cy.getDataCy('subcriber-result-count').should('have.text', '4')
+    cy.getDataCy('subscriber-result-count').should('have.text', '4')
   })
 
   it('should inactivate subscriber', () => {
     cy.visit("#tilaajat/")
-    cy.getDataCy('subcriber-result-count').should('have.text', '4')
+    cy.getDataCy('subscriber-result-count').should('have.text', '4')
     //remove DEMO tilaaja 4 account
     cy.getDataCy('result').eq(3).click()
     cy.getDataCy('inactivate-subscriber-button:first').click()
     cy.getDataCy('inactivate-subscriber-dialog').should('be.visible')
     cy.getDataCy('inactivate-subscriber-dialog:last buttons confirm-inactivate').click()
     cy.getDataCy('result').eq(3).should('have.class', 'inactive')
-    cy.getDataCy('subcriber-result-count').should('have.text', '3')
+    cy.getDataCy('subscriber-result-count').should('have.text', '3')
   })
 
   it('inactivated subscribers should not show in classification buyer and billing options', () => {
@@ -34,7 +34,7 @@ describe('Subscribers', () => {
 
   it('adding a subscriber is possible', () => {
     cy.visit("#tilaajat/")
-    cy.getDataCy('subcriber-result-count').should('have.text', '3')
+    cy.getDataCy('subscriber-result-count').should('have.text', '3')
     cy.getDataCy('new-subscriber-button').click()
     cy.getDataCy('save-subscriber-button:visible').should('be.disabled')
     cy.getDataCy('subscriber-checkbox:visible').click()
@@ -50,7 +50,7 @@ describe('Subscribers', () => {
     cy.select2('.select2-container.x-emailaddresses', 'test@example.com')
     cy.getDataCy('save-subscriber-button:visible').should('be.enabled')
     cy.getDataCy('save-subscriber-button:visible').click()
-    cy.getDataCy('subcriber-result-count').should('have.text', '4')
+    cy.getDataCy('subscriber-result-count').should('have.text', '4')
   })
 })
 

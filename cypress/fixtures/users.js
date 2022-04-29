625d0d5962e576f56f888e8f85d1cfc6e9ee44e1
Nuutti Rintala <nuutti.rintala@reaktor.fi>
2022-04-29 16:54:58 +0300

    Add accounts with testusers

--- cypress/fixtures/users.js
+++ cypress/fixtures/users.js
@@ -1,5 +1,5 @@
 module.exports = [
-  {username: 'ROOT', active: true, password: 'root', role: 'root', name: 'root', emails: ['root@fake-meku.fi']},
-  {username: 'KAVI', active: true, password: 'kavi', role: 'kavi', name: 'kavi', emails: ['kavi@fake-meku.fi']},
-  {username: 'USER', active: true, password: 'user', role: 'user', name: 'user', emails: ['user@fake-meku.fi']}
+  {"_id": "61b87f77fbbc1c56d7967f1d", username: 'ROOT', active: true, password: 'root', role: 'root', name: 'root', emails: ['root@fake-meku.fi']},
+  {"_id": "61b87f77fbbc1c56d7967f1e", username: 'KAVI', active: true, password: 'kavi', role: 'kavi', name: 'kavi', emails: ['kavi@fake-meku.fi']},
+  {"_id": "61b87f77fbbc1c56d7967f1c", username: 'USER', active: true, password: 'user', role: 'user', name: 'user', emails: ['user@fake-meku.fi']}
 ]

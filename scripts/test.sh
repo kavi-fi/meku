e57adda91aeb1ce9f3cbf254c607cd537c6cc19e
Mikko Peltonen <mikko.peltonen@reaktor.com>
2022-06-03 12:05:32 +0300

    Improve test start script output

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,17 +2,27 @@
 
 if [ ! -f ./node_modules/.bin/cypress ]; then echo "Run 'npm install' first"; exit 1; fi
 
+echo "Running ESLint..."
 ./node_modules/.bin/eslint client/js server shared cypress/integration
 if [ "$?" != 0 ]; then
   exit "$?"
 fi
 
 PORT=4000
-NODE_ENV=test node server/app.js &
+echo "Starting server for browser tests..."
+NODE_ENV=test SENDGRID_APIKEY="SG.dummy-apikey" node server/app.js &
 APP_PID=$!
 
+printf "Waiting for the server to listen.."
+until $(curl --output /dev/null --silent --head http://localhost:4000); do
+    printf '.'
+    sleep 2
+done
+
+echo "Importing test fixtures..."
 NODE_ENV=test node cypress/fixtures
 if [ "$?" = 0 ]; then
+  echo "Starting Cypress run..."
   CYPRESS_BASE_URL=http://localhost:$PORT/ node_modules/.bin/cypress run
 fi
 
